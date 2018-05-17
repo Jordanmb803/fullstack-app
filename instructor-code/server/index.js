b5660e7f5add340a9511990416b8c71bafb30f99
Jordan Buonforte <jordan@Jordans-MacBook-Air.local>
2018-05-17 11:43:59 -0600

    user login successful

--- instructor-code/server/index.js
+++ instructor-code/server/index.js
@@ -10,10 +10,17 @@ const {
     SESSION_SECRET,
     DOMAIN,
     CLIENT_ID,
-    CLIENT_SECRET
+    CLIENT_SECRET,
+    CALLBACK_URL,
+    CONNECTION_STRING
 } = process.env;
 
 const app = express();
+
+massive(CONNECTION_STRING).then((db) => {
+    console.log('Connected to DB');
+    app.set('db', db);
+})
 
 // order in important
 // session
@@ -30,8 +37,24 @@ passport.use(new Auth0Strategy({
     domain: DOMAIN,
     clientID: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
-    callbackURL: '',
+    callbackURL: CALLBACK_URL,
     scope: 'openid profile'
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    let db = app.get('db');
+    
+}))
+
+passport.serializeUser( (profile, done) => {
+    done(null, profile);
+})
+passport.deserializeUser( (profile, done) => {
+    // whatever we pass out, ends up on req object as req.user
+    done(null, profile);
+})
+
+app.get('/login', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000'
 }))
 
 
--- server/index.js
+++ server/index.js
@@ -10,10 +10,16 @@ const {
     SESSION_SECRET,
     DOMAIN,
     CLIENT_ID,
-    CLIENT_SECRET
+    CLIENT_SECRET,
+    CALLBACK_URL,
+    CONNECTION_STRING
 } = process.env
 
 const app = express()
+
+massive(CONNECTION_STRING).then( (db) => {
+    app.set('db', db)
+})
 
 // order is important
 app.use(session({
@@ -29,8 +35,26 @@ passport.use(new Auth0Strategy({
     domain: DOMAIN,
     clientID: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
-    callbackURL: '',
+    callbackURL: CALLBACK_URL,
     scope: 'openid profile'
+//  below is the function that runs when a user successfully logs on
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    let db = app.get('db')
 }))
 
-app.listen(SERVER_PORT, () => {console.log(`${SERVER_PORT} now listening`)} )
+passport.serializeUser((profile, done) => {
+    done(null, profile)
+})
+
+passport.deserializeUser(() => {
+    // whatever we pass out, ends up on the req object as req.user
+    done(null, profile)
+})
+
+app.get('/login', passport.authenticate('auth0'))
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000',
+    failureRedirect: '/login'
+}))
+
+app.listen(SERVER_PORT, () => {console.log(`Port ${SERVER_PORT} now listening`)} )
