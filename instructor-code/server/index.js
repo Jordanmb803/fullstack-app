56d78eceff40903875942eafda21b577f9f2a9e6
Jordan Buonforte <jordan@Jordans-MacBook-Air.local>
2018-05-17 10:47:03 -0600

    passport initialized

--- instructor-code/server/index.js
+++ instructor-code/server/index.js
@@ -6,10 +6,34 @@ const express = require('express')
     , massive = require('massive');
 
 const {
-    SERVER_PORT
+    SERVER_PORT,
+    SESSION_SECRET,
+    DOMAIN,
+    CLIENT_ID,
+    CLIENT_SECRET
 } = process.env;
 
 const app = express();
+
+// order in important
+// session
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+// must come after session
+app.use(passport.initialize());
+//must come after initialize middleware;
+app.use(passport.session());
+passport.use(new Auth0Strategy({
+    domain: DOMAIN,
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: '',
+    scope: 'openid profile'
+}))
+
 
 app.listen(SERVER_PORT, () => {
     console.log(`Listening on port: ${SERVER_PORT}`)
--- server/index.js
+++ server/index.js
@@ -6,11 +6,31 @@ const express = require('express')
     , Auth0Strategy = require('passport-auth0')
 
 const {
-    SERVER_PORT
+    SERVER_PORT,
+    SESSION_SECRET,
+    DOMAIN,
+    CLIENT_ID,
+    CLIENT_SECRET
 } = process.env
 
-
-
 const app = express()
+
+// order is important
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+// must come after session
+app.use(passport.initialize())
+// must come after initialize middleware
+app.use(passport.session())
+passport.use(new Auth0Strategy({
+    domain: DOMAIN,
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: '',
+    scope: 'openid profile'
+}))
 
 app.listen(SERVER_PORT, () => {console.log(`${SERVER_PORT} now listening`)} )
